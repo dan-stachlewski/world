b0a896b31e4dcace0b19b643cdc524516cef3122
dan-stachlewski <dan.stachlewski@gmail.com>
2015-06-01 14:24:54 +1000

    index.php
    This is the landing page of www.worldinpictures.org

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="World in Pictures">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Home</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="author" href="humans.txt">
+    </head>
+    <body>
+        <h1>Welcome to World in Pictures</h1>
+        <script src="js/main.js"></script>
+    </body>
+</html>
