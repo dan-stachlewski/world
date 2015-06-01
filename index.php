08a3149a18f11822f48661de13198d69cca13d49
dan-stachlewski <dan.stachlewski@gmail.com>
2015-06-01 14:40:51 +1000

    Display records
    Added the code that will display the records from the database on the index.php page

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php 
+
+//include the code that connects to the database and creates a $db connection object
+require_once('includes/db-connect.php');
+
+
+?>
+
 <!doctype html>
 <html>
     <head>
@@ -10,6 +18,23 @@
     </head>
     <body>
         <h1>Welcome to World in Pictures</h1>
+
+		<section><!-- This section displays world region data from the world_pic database -->
+			
+			<?php 
+
+			if ($numRowsCountry > 0) {
+				echo '<b><a href="Locations.php?id=AU>"Australia</a></b><br/>';
+			} else {
+				echo 'No records found!';
+			}
+
+
+			?>
+
+		</section><!-- /END section -->
+
+
         <script src="js/main.js"></script>
     </body>
 </html>
