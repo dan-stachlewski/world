0cf8bfc5a4329ff3814b21238df26d4eb07cac23
dan-stachlewski <dan.stachlewski@gmail.com>
2015-06-24 14:00:03 +1000

    Just some things to finalise if I dont run out of time

---
+++ website-checklist.php
@@ -0,0 +1,60 @@
+<?php
+
+/**************************************************
+ * REMINDER OF TASKS TO COMPLETE BEFOR HANDING IN:
+ **************************************************
+ * WEBPAGE: countries.php
+ * LINE #: 26 | fix header to: header("Location: http://localhost/worldinpic/index.php");
+ *************************************************
+ * LABLE ALL THE HTML CODE BLOCKS START AND END
+ **************************************************
+ * WEBPAGE: public_register.php & photographer_register.php
+ * LINE #: 00 | REMOVE THE OLD VALIDATION CODE BY THE FORM INPUT BOXES
+ * RE-POSITION ERROR OUTPUT
+ **************************************************
+ * WEBPAGE: registration pages -
+ * LINE #: 00 | add a success and thankyou [persons name] give them the option of where to go next and not be sent to the photographs page only to be re-directed because there is NO ID in the URL
+ **************************************************
+ * WEBPAGE:
+ * LINE #: 00 |
+ **************************************************
+ * WEBPAGE:
+ * LINE #: 00 |
+ **************************************************
+
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
